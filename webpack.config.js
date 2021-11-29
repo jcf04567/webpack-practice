137f5edcf59cdac9fcaba110e96925d07a0c80f7
kamiya <78120565+jcf04567@users.noreply.github.com>
2021-11-30 08:35:25 +0900

    Use Asset Modules

--- webpack.config.js
+++ webpack.config.js
@@ -24,14 +24,18 @@ module.exports = {
       },
       {
         test:/\.(png|bmp|jpg)/,
+        type:'asset/resource',
+        generator:{
+          filename:'images/[name][ext]',
+        },
         use:[
-          {
-            loader:'file-loader',
-            options:{
-              esModule:false,
-              name:'images/[name].[ext]',
-            },
-          },
+          // {
+          //   loader:'file-loader',
+          //   options:{
+          //     esModule:false,
+          //     name:'images/[name].[ext]',
+          //   },
+          // },
         ],
       },
     ],
